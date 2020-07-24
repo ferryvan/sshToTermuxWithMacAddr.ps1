e4d20ef60cedb97e58c7b21a03c76e13f3cfd9e3
ferryvan <1156709213@qq.com>
2020-07-25 05:31:20 +0800

    "fix: regex bug"

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Connect to your termux without ip within LAN
 A Powershell script to ssh to your termux using MAC Addr
 
 1. Search MAC Addr in Arp Cache
-2. Try host discovey on 192.168.1.2-254 (using nmap)
+2. Try host discovey on 192.168.1.2-253 (using nmap)
 3. Ssh ip -p port
 
 Need: 
--- sshtermux.ps1
+++ sshtermux.ps1
@@ -1,8 +1,8 @@
 #SETTINGS
-$MAC = "74-59-09-32-1c-ga"
-$BIG_MAC = "74:59:09:32:1C:GA"
+$MAC = "74-59-09-32-1c-ea"
+$BIG_MAC = "74:59:09:32:1C:EA"
 $port = "8022" # ssh port,default 8022 for Termux sshd
-# Search Under 192.168.1.2-254
+# Search Under 192.168.1.2-253
 # Retry once
 
 Write-Output "MAC: ${MAC}"
@@ -23,15 +23,15 @@ if ($case) {
 else 
 {
     Write-Output "Device not found in Arp Cache"
-    Write-Output "Trying host discovey on 192.168.1.2-254"
+    Write-Output "Trying host discovey on 192.168.1.2-253"
     Write-Output "---------------------------------------"
     $str2 = nmap -n -p ${port} -PS 192.168.1.2-253
     $str2
-    #nmap -n -Pn -T4 -p ${port} 192.168.1.2-254
+    #nmap -n -Pn -T4 -p ${port} 192.168.1.2-253
     Write-Output "---------------------------------------"
     #$str2 = arp -a
     #$str2
-    $pattern2="\b(192\.168\.1\.[0-9]+){1}[\s\S]*(${BIG_MAC}){1}"
+    $pattern2="\b(192\.168\.1\.[0-9]+){1}((?!Nmap).)*(${BIG_MAC}){1}"
     $str2 = [system.String]::Join("", $str2)
     $case2 = $str2 -match $pattern2
     if ($case2){
