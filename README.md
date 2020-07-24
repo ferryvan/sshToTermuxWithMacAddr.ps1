d6cd8c108b3a6ca8f08671f75923865d150fb8d4
ferryvan <1156709213@qq.com>
2020-07-25 05:05:54 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # sshToTermuxWithMacAddr.ps1
 Connect to your termux without ip within LAN
+---
+
+A Powershell script to ssh to your termux using MAC Addr
+
+1. Search MAC Addr in Arp Cache
+2. Try host discovey on 192.168.1.2-254 (using nmap)
+3. Ssh ip -p port
+
+Need: 
+- nmap for windows >= 7.80 (https://nmap.org)
+- powershell >= 7 (https://microsoft.com/PowerShell)
+
+Use: 
+- change SETTINGS and ./sshtermux.ps1
+- make sure that your termux sshd is online
+
+enjoy!
+```
+
+                             o==)_\
+|                              ',====_          NO BUG
+`========                        \ \  \_____    GOD BLESS ME
+(________)/~-----||-----.         \ \  `\  ||
+   _\_\__/_______||______\         \ \   `\||
+  (||__________________   \        |  |)   `\_
+   /          /_~_///  \  /        / /        `-_
+  /   -==============- / /        / / ___________\_
+ /_____________==//___/_/________/_/___===========|
+ _//|  ( () )/ |~`-----------------'  |  ( ()')  |
+'-'  \_ `--' _/                        \_ `--' _/
+       `----'                            `----'
+```
---
+++ sshtermux.ps1
@@ -0,0 +1,45 @@
+#SETTINGS
+$MAC = "74-59-09-32-1c-ga"
+$BIG_MAC = "74:59:09:32:1C:GA"
+$port = "8022" # ssh port,default 8022 for Termux sshd
+# Search Under 192.168.1.2-254
+# Retry once
+
+Write-Output "MAC: ${MAC}"
+#sudo arp -d
+$str = arp -a
+# $str
+$str = [system.String]::Join("", $str)
+$startpattern = '\b(192\.168\.1\.[0-9]+\b)[ ]+'
+$pattern = $startpattern + $MAC
+
+$case = $str -match $pattern
+if ($case) {
+    $ip = $Matches[1]
+    Write-Output "Device found at ${ip}"
+    Write-Output "Starting ssh connect"
+    ssh ${ip} -p ${port}
+}
+else 
+{
+    Write-Output "Device not found in Arp Cache"
+    Write-Output "Trying host discovey on 192.168.1.2-254"
+    Write-Output "---------------------------------------"
+    $str2 = nmap -n -p ${port} -PS 192.168.1.2-253
+    $str2
+    #nmap -n -Pn -T4 -p ${port} 192.168.1.2-254
+    Write-Output "---------------------------------------"
+    #$str2 = arp -a
+    #$str2
+    $pattern2="\b(192\.168\.1\.[0-9]+){1}[\s\S]*(${BIG_MAC}){1}"
+    $str2 = [system.String]::Join("", $str2)
+    $case2 = $str2 -match $pattern2
+    if ($case2){
+        $ip = $Matches[1]
+        Write-Output "Device found at ${ip}"
+        Write-Output "Starting ssh connect"
+        ssh ${ip} -p ${port}
+    }else {
+        Write-Output "Device not found"
+    }
+}
